1987a88d793ba7991aabf6efae98cbe76247786f
wei <backlu84@gmail.com>
2023-02-22 20:43:47 +0800

    clac avg length of reviews

--- read.py
+++ read.py
@@ -6,9 +6,11 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
+print('檔案讀取完了，總共有' ,len(data),'筆資料')
 
-
-print(data[0])
-print('-------------')
-print(data[1])
+sum_len = 0
+for d in data:
+	len(d)
+	
+	sum_len = sum_len + len(d)
+print('平均是',sum_len/len(data))
